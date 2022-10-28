34075dbe8bcd0d24991436f704af70ff50315e89
zakumann <samuel742able@gmail.com>
2022-10-29 00:37:55 +0900

    글 목록 출력하기

--- nodejs/fileread.js
+++ nodejs/fileread.js
@@ -1,4 +1,6 @@
-var fs = require('fs');
-fs.readFile('sample.txt','utf8',function(err, data){
-    console.log(data);
+import { readFile } from 'node:fs';
+
+readFile('sample.txt', (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
